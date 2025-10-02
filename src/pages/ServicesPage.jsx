4b41f8ee0540cd262a448b5dfb2dca41c18065d3
aynalhaque <aynalhaque7411@gmail.com>
2025-10-02 01:45:59 -0700

    chanfge

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export default function ServicesPage() {
   return (
     <div>
-      <h1>Our Services</h1>
+      <h1>Monir's Services</h1>
       <ul>
-        <li>General Health Checkup</li>
-        <li>Maternal & Child Care</li>
-        <li>Vaccination Programs</li>
+        <li>General health Advice</li>
+        <li>Observe Health condition day by day</li>
+        <li>Sex and skin disease Advice</li>
+        <li>Child health and growth monitoring</li>
+        <li>Mental councheling</li>
       </ul>
     </div>
   );
