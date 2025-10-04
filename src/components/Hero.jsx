1b817e2b2fcf58c0c4b15e32f1f1d9929a93025b
aynalhaque <aynalhaque7411@gmail.com>
2025-10-04 03:59:21 -0700

    image added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Hero.css";
+import clinicImage from "../assets/monir.jpg"; 
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
+         <img src={clinicImage} alt="Clinic" className="hero-img" />
         <h1>Welcome to Capricious Monir’s Clinic</h1>
         <p>Serving the Rohingya community with compassion and care.</p>
         <a href="/appointment" className="btn">Book Appointment</a>
